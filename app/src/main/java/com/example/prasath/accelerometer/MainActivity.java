edc14821671fe8fc960d226694ab5a7fd2afbb90
Prasath Rajan <prasathrajan555@gmail.com>
2016-03-02 20:11:01 +0530

    great leaps in accelerometer

--- app/src/main/java/com/example/prasath/accelerometer/MainActivity.java
+++ app/src/main/java/com/example/prasath/accelerometer/MainActivity.java
@@ -1,17 +1,39 @@
 package com.example.prasath.accelerometer;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+    Sensor acc;
+    SensorManager sm;
+    TextView acceleration;
+
+    static float[] previous = new float[3];
+    static float[] delta = new float[3];
+    static int jerkcount=0;
+    static float jerk=0;
+    static float[] weights = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        acc = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sm.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+
+        acceleration=(TextView)findViewById(R.id.acceleration);
+
+
     }
 
 
@@ -35,5 +57,31 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+
+
+    }
+
+
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        jerk = 0;
+
+        for(int i=0; i<3; i++){
+            delta[i] = event.values[i] - previous[i];
+            jerk+=(delta[i]*delta[i])*weights[i]*weights[i];
+            previous[i] = event.values[i];
+
+        }
+        if(jerk>5000)
+            jerkcount++;
+        else{
+            for(int i=0; i<3; i++)
+                weights[i] = event.values[i];
+        }
+        acceleration.setText("\n" + jerk + "\n" + (jerkcount/3) + "\n" + event.values[0] + "\n " + event.values[1] + "\n " + event.values[2]);
     }
 }
